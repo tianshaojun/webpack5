4b1c35bc647ed17e4f535e9058aced4b15f2cdc3
田少军 <tianshaojun@192.168.3.14>
2021-12-08 22:51:11 +0800

    20211208晚上学习

---
+++ 06-manage-assets/src/index.js
@@ -0,0 +1,30 @@
+import helloWorld from './hello-world'
+import imgsrc from './assets/1.png'
+import logoSvg from './assets/1.svg'
+import exampleText from './assets/example.txt'
+import jpgMap from './assets/1.jpg'
+import './style.css'
+import './style.less'
+
+helloWorld();
+
+const img = document.createElement('img')
+img.src = imgsrc
+document.body.appendChild(img)
+
+const img2 = document.createElement('img')
+img2.style.cssText = 'width:600px;height:200px'
+img2.src = logoSvg
+document.body.appendChild(img2)
+
+const block = document.createElement('div')
+block.style.cssText = 'width:200px;height:200px;background:aliceblue'
+block.textContent = exampleText
+document.body.appendChild(block)
+
+const img3 = document.createElement('img')
+img3.style.cssText = 'width:600px;height:240px;display:block'
+img3.src = jpgMap
+document.body.appendChild(img3)
+
+document.body.classList.add('hello')
---
+++ 06-manage-assets/webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+
+    //必须是绝对路径
+    path: path.resolve(__dirname, './dist'),
+
+    //打包前清理dist文件夹
+    clean: true,
+    // assetModuleFilename: 'images/test.png'
+    assetModuleFilename: 'images/[contenthash][ext]',
+  },
+
+  // mode: 'development',
+  mode: 'production',
+
+  //在开发模式下追踪代码
+  devtool: 'inline-source-map',
+
+  plugins: [
+    //实例化html-webpack-plugin插件
+    new HtmlWebpackPlugin({
+      template: './index.html', //打包生成的文件模板
+      filename: 'app.html', //打包生成的文件名称
+      inject: 'body', //资源文件注入模板的位置
+    }),
+
+    //抽离css插件
+    new MiniCssExtractPlugin({
+      filename: './styles/[contenthash].css',
+    }),
+  ],
+
+  devServer: {
+    static: './dist',
+  },
+
+  //配置资源文件
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        type: 'asset/resource',
+        //优先级高于assetModuleFilename
+        generator: {
+          filename: 'images/[contenthash][ext]',
+        },
+      },
+
+      {
+        test: /\.svg$/,
+        type: 'asset/inline',
+      },
+
+      {
+        test: /\.txt$/,
+        type: 'asset/source',
+      },
+
+      {
+        test: /\.jpg$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            // maxSize: 4 * 1024,
+            maxSize: 4 * 1024 * 1024,
+          },
+        },
+      },
+
+      {
+        test: /\.(css|less)$/,
+        // use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+
+  //优化配置
+  optimization:{
+    minimizer:[
+      new CssMinimizerWebpackPlugin(),
+    ]
+  }
+}
