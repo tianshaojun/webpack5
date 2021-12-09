5926e62ada88977b1ea5fbfe2f4a31af3b687d0e
田少军 <tianshaojun@192.168.3.14>
2021-12-09 21:43:15 +0800

    20211209晚上babel-loader学习

---
+++ 07-babel-loader/src/index.js
@@ -0,0 +1,54 @@
+import helloWorld from './hello-world'
+import imgsrc from './assets/1.png'
+import logoSvg from './assets/1.svg'
+import exampleText from './assets/example.txt'
+import jpgMap from './assets/1.jpg'
+import './style.css'
+import './style.less'
+import Data from './assets/data.xml'
+import Notes from './assets/data.csv'
+import toml from './assets/data.toml'
+import yaml from './assets/data.yaml'
+import json from './assets/data.json5'
+
+console.log(Data)
+console.log(Notes)
+
+console.log(toml.title)
+console.log(toml.owner.name)
+
+console.log(yaml,'yaml')
+// console.log(yaml.owner.name)
+
+console.log(json.title)
+console.log(json.owner.name)
+
+helloWorld();
+
+const img = document.createElement('img')
+img.src = imgsrc
+document.body.appendChild(img)
+
+const img2 = document.createElement('img')
+img2.style.cssText = 'width:600px;height:200px'
+img2.src = logoSvg
+document.body.appendChild(img2)
+
+const block = document.createElement('div')
+block.style.cssText = 'width:200px;height:200px;background:aliceblue'
+block.classList.add('block-bg')
+block.textContent = exampleText
+document.body.appendChild(block)
+
+const img3 = document.createElement('img')
+img3.style.cssText = 'width:600px;height:240px;display:block'
+img3.src = jpgMap
+document.body.appendChild(img3)
+
+document.body.classList.add('hello')
+
+const span = document.createElement('span')
+span.classList.add('icon')
+span.innerHTML = '&#xe85c;'
+document.body.appendChild(span)
+
---
+++ 07-babel-loader/webpack.config.js
@@ -0,0 +1,142 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const toml = require('toml')
+const yaml = require('yamljs')
+const json5 = require('json5')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+
+    //必须是绝对路径
+    path: path.resolve(__dirname, './dist'),
+
+    //打包前清理dist文件夹
+    clean: true,
+    // assetModuleFilename: 'images/test.png'
+    assetModuleFilename: 'images/[contenthash][ext]',
+  },
+
+  mode: 'development',
+  // mode: 'production',
+
+  //在开发模式下追踪代码
+  devtool: 'inline-source-map',
+
+  plugins: [
+    //实例化html-webpack-plugin插件
+    new HtmlWebpackPlugin({
+      template: './index.html', //打包生成的文件模板
+      filename: 'app.html', //打包生成的文件名称
+      inject: 'body', //资源文件注入模板的位置
+    }),
+
+    //抽离css插件
+    new MiniCssExtractPlugin({
+      filename: './styles/[contenthash].css',
+    }),
+  ],
+
+  devServer: {
+    static: './dist',
+  },
+
+  //配置资源文件
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        type: 'asset/resource',
+        //优先级高于assetModuleFilename
+        generator: {
+          filename: 'images/[contenthash][ext]',
+        },
+      },
+
+      {
+        test: /\.svg$/,
+        type: 'asset/inline',
+      },
+
+      {
+        test: /\.txt$/,
+        type: 'asset/source',
+      },
+
+      {
+        test: /\.jpg$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            // maxSize: 4 * 1024,
+            maxSize: 4 * 1024 * 1024,
+          },
+        },
+      },
+
+      {
+        test: /\.(css|less)$/,
+        // use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader'],
+      },
+
+      {
+        test: /\.xml$/,
+        use: ['xml-loader'],
+      },
+
+      {
+        test: /\.toml$/,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-runtime']],
+          },
+        },
+      },
+    ],
+  },
+
+  //优化配置
+  optimization: {
+    minimizer: [new CssMinimizerWebpackPlugin()],
+  },
+}
